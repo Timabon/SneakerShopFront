e3736e5d3c2bf0dcd3c06e7db595389c7f547d55
timabondarcuk <timabondarcuk@gmail.com>
2025-02-07 04:50:01 +0100

    fixed namings

--- src/components/ListProductsComponent.jsx
+++ src/components/ListProductsComponent.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
 function ListProductsComponent() {
-  const [products, setProducts] = useState([]); // State to store products
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("https://sneakershopback.onrender.com/api/v1/products/all") // Replace with your actual API endpoint
+    fetch("https://sneakershopback.onrender.com/api/v1/products/all")
       .then((response) => response.json())
       .then((data) => setProducts(data))
       .catch((error) => console.error("Error fetching products:", error));
@@ -14,13 +14,13 @@ function ListProductsComponent() {
     <div className="container mt-4">
       <div className="row">
         {products.map((product) => (
-          <div key={product.id} className="col-md-4"> {/* 3 products per row */}
+          <div key={product.productId} className="col-md-4"> {/* 3 products per row */}
             <div className="card" style={{ width: "18rem" }}>
-              <img src={product.imageUrl} className="card-img-top" alt={product.name} />
+              <img src={product.imageUrls[0]} className="card-img-top" alt={product.productName} />
               <div className="card-body">
-                <h5 className="card-title">{product.name}</h5>
-                <p className="card-text">${product.price}</p>
-                <a href={`/product/${product.id}`} className="btn btn-primary">View Product</a>
+                <h5 className="card-title">{product.productName}</h5>
+                <p className="card-text">${product.productPrice}</p>
+                <a href={`/product/${product.productId}`} className="btn btn-primary">View Product</a>
               </div>
             </div>
           </div>
