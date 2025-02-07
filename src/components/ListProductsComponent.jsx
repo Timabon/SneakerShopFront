5fd5e64db3272885aa3eaebbf1fe44a3edc9635c
timabondarcuk <timabondarcuk@gmail.com>
2025-02-07 06:52:57 +0100

    corrected names of Product Properties

--- src/components/ListProductsComponent.jsx
+++ src/components/ListProductsComponent.jsx
@@ -16,11 +16,11 @@ function ListProductsComponent() {
         {products.map((product) => (
           <div key={product.id} className="col-md-4"> {/* 3 products per row */}
             <div className="card" style={{ width: "18rem" }}>
-              <img src={product.imageUrls} className="card-img-top" alt={product.name} />
+              <img src={product.imageUrls} className="card-img-top" alt={product.productName} />
               <div className="card-body">
-                <h5 className="card-title">{product.name}</h5>
-                <p className="card-text">${product.price}</p>
-                <a href={`/product/${product.id}`} className="btn btn-primary">View Product</a>
+                <h5 className="card-title">{product.productName}</h5>
+                <p className="card-text">${product.productPrice}</p>
+                <a href={`/product/${product.productId}`} className="btn btn-primary">View Product</a>
               </div>
             </div>
           </div>
